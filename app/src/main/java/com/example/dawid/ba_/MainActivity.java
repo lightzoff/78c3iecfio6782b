41dd5a7325b9e61ec8fa280234a16279844291de
Dawid <gutladavid@gmail.com>
2016-04-02 18:48:14 +1300

    Test
    initial commit

---
+++ app/src/main/java/com/example/dawid/ba_/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.dawid.ba_;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn = (Button) findViewById(R.id.btnViewShows);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ViewShowsActivity();
+            }
+
+        });
+    }
+    public void ViewShowsActivity(){
+        Intent in = new Intent(this,ViewShowsActivity.class);
+        this.startActivity(in);
+    }
+    //comment david david david david
+}
