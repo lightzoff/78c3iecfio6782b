541fab9ff079afdf9370d8b94d6ce6ea9e4f4df7
jozellaroque <jozellaroque2012@gmail.com>
2016-04-20 14:41:39 +1200

    Jozella's Commit, Menu Pages

---
+++ app/src/main/java/com/example/dawid/ba_/BuyTicket.java
@@ -0,0 +1,33 @@
+package com.example.dawid.ba_;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by jozel_000 on 20/04/2016.
+ */
+public class BuyTicket extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn = (Button) findViewById(R.id.btnViewShows);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ViewShowsActivity();
+            }
+
+        });
+    }
+
+    public void ViewShowsActivity() {
+        Intent in = new Intent(this, Home.class);
+        this.startActivity(in);
+    }
+}
+
